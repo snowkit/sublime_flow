4958ccdaf3f1182f74c7cbebfb76f3031681a4ab
underscorediscovery <sven@underscorediscovery.com>
2014-08-29 17:10:02 -0230

    initial commit

---
+++ flow.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+import sys, os
+
+import sublime, sublime_plugin
+
+#plugin location
+plugin_file = __file__
+plugin_filepath = os.path.realpath(plugin_file)
+plugin_path = os.path.dirname(plugin_filepath)
+
+
+# print("hello flow")
